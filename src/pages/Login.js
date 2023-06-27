bff8ba44083d811aedc2a4dc33d348b108e5c4f9
Dianakariuki <dianakariuki842@gmail.com>
2023-06-27 10:59:45 +0300

    ft: create  a function to sign in with a popup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,15 @@
 import React from "react";
 import {auth,provider} from "../firebase-configure";
+import{signInWithPopup } from  "firebase/auth ";
 
 function Login() {
-    return (
+  const signInWithGoogle = () =>{
+    signInWithPopup(auth,provider).then((result)=>{
+
+    })
+  };
+  
+  return (
       <div className="loginPage">
        <p>Sign With Google to Continue</p>
        <button className="login-with-google-btn">Sign in with Google</button>
