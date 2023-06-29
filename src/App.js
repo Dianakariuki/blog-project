812d1984aa0d2e29a9d9e661815ebb6b38ee1d74
Dianakariuki <dianakariuki842@gmail.com>
2023-06-29 16:40:11 +0300

    ft:sign out authentication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 
 import './App.css';
-import {  BrowserRouter as Router,  Routes,  Route,Link} from "react-router-dom";
+import {  BrowserRouter as Router,  Routes,  Route,Link, useNavigate} from "react-router-dom";
 import {useState} from 'react';
 import React  from 'react'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import CreatePost from './pages/CreatePost';
+import { signOut } from 'firebase/auth';
+import { auth } from "./firebase-configure";
+
 
 function App() {
+  
   const [isAuth, setIsAuth] = useState(false);
+ 
+
+  const signUserOut = () => {
+    signOut(auth).then(() =>{
+      localStorage.clear();
+      setIsAuth(false);
+     window.location.pathname ="/login";
+    });
+  };
+
   return (
     <Router>
       <nav>
         <Link to ="/">Home</Link>
-        <Link to ="/login">Login</Link>
         <Link to ="/createpost">CreatePost</Link>
+        {!isAuth ? <Link to ="/login">Login</Link>:<button onClick={signUserOut}>Log Out</button>}
+       
       </nav>
       <Routes>
       <Route path="/" element={<Home/>} />
