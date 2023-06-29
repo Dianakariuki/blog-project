668a0630825f939042fa2ebaef1f8481db8f2348
Dianakariuki <dianakariuki842@gmail.com>
2023-06-29 16:45:13 +0300

    ft: Createpost html

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -2,7 +2,19 @@ import React from "react";
 function CreatePost() {
     return (
       <div className="createPostPage">
-        <div class
+        <div className="cpContainer">
+          <h1>Create A post</h1>
+          <div className="inputGp">
+            <label>Title:</label>
+            <input placeholder="Title..."/>
+            </div>
+            <div className="inputGp">
+              <label>Post:</label>
+              <textarea placeholder="Post...">
+                
+              </textarea>
+          </div>
+        </div>
       </div>
     );
   }
